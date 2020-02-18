3373f722f185742e6d78c4b7e2d4a59f715f0fba
Dustin Specker <DustinSpecker@DustinSpecker.com>
2020-02-17 19:13:00 -0600

    refactor(main): create ghostdog CLI
    Move previous functionality to `build` action.

--- cmd/ghostdog/main.go
+++ cmd/ghostdog/main.go
@@ -4,18 +4,34 @@ import (
 	"log"
 	"os"
 
+	"github.com/urfave/cli/v2"
+
 	"github.com/dustinspecker/ghostdog/internal/build"
 )
 
 func main() {
-	buildFilePath := "BUILD"
+	app := &cli.App{
+		Name:  "ghostdog",
+		Usage: "improve your build process",
+		Commands: []*cli.Command{
+			{
+				Name:  "build",
+				Usage: "build projects using BUILD files",
+				Action: func(c *cli.Context) error {
+					buildFilePath := "BUILD"
 
-	buildFileData, err := os.Open(buildFilePath)
-	if err != nil {
-		log.Fatal(err)
+					buildFileData, err := os.Open(buildFilePath)
+					if err != nil {
+						return err
+					}
+
+					return build.RunBuildFile(buildFilePath, buildFileData)
+				},
+			},
+		},
 	}
 
-	if err = build.RunBuildFile(buildFilePath, buildFileData); err != nil {
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
