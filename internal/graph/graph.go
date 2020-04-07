dustinspecker/ghostdog

commit f1e29f4d186f5a8ed7312957e72132f8cd5d24c5

refactor(internal/analyze): take buildTarget to return rules to use

internal/graph/graph.go
@@ -16,34 +16,20 @@ func GetGraph(fs afero.Fs, buildFileName, buildTarget string, outputFile io.Writ
 		return err
 	}
 
-	rules, err := analyze.GetRules(buildFileName, buildFile)
+	rules, err := analyze.GetRules(buildFileName, buildFile, buildTarget)
 	if err != nil {
 		return err
 	}
 
-	dotGraph, err := getDotGraph(rules, buildTarget)
-	if err != nil {
-		return err
-	}
+	dotGraph := getDotGraph(rules)
 
 	_, err = outputFile.Write([]byte(dotGraph))
 
 	return err
 }
 
-func getDotGraph(rules map[string]*rule.Rule, buildTarget string) (string, error) {
+func getDotGraph(rules map[string]*rule.Rule) string {
 	dagString := "digraph g {\n"
-
-	if buildTarget != "all" {
-		targetRule, ok := rules[buildTarget]
-		if !ok {
-			return "", fmt.Errorf("target %s not found", buildTarget)
-		}
-
-		rules = map[string]*rule.Rule{
-			targetRule.Name: targetRule,
-		}
-	}
 
 	for _, rule := range rules {
 		for _, child := range rule.Children {
@@ -53,5 +39,5 @@ func getDotGraph(rules map[string]*rule.Rule, buildTarget string) (string, error
 
 	dagString = dagString + "}"
 
-	return dagString, nil
+	return dagString
 }

internal/analyze/analyze.go
@@ -1,6 +1,7 @@
 package analyze
 
 import (
+	"fmt"
 	"io"
 
 	"go.starlark.net/starlark"
@@ -10,7 +11,7 @@ import (
 	"github.com/dustinspecker/ghostdog/internal/rule"
 )
 
-func GetRules(buildFileName string, buildFileData io.Reader) (map[string]*rule.Rule, error) {
+func GetRules(buildFileName string, buildFileData io.Reader, buildTarget string) (map[string]*rule.Rule, error) {
 	thread := &starlark.Thread{Name: "ghostdog-main"}
 
 	rulesDag := dag.NewDag()
@@ -41,5 +42,15 @@ func GetRules(buildFileName string, buildFileData io.Reader) (map[string]*rule.R
 		}
 	}
 
-	return rulesDag.Rules, nil
+	if buildTarget == "all" {
+		return rulesDag.GetSources(), nil
+	}
+
+	if _, ok := rulesDag.Rules[buildTarget]; !ok {
+		return nil, fmt.Errorf("target %s not found", buildTarget)
+	}
+
+	return map[string]*rule.Rule{
+		buildTarget: rulesDag.Rules[buildTarget],
+	}, nil
 }

internal/analyze/analyze_test.go
@@ -1,11 +1,8 @@
 package analyze
 
 import (
-	"reflect"
 	"strings"
 	"testing"
-
-	"github.com/dustinspecker/ghostdog/internal/rule"
 )
 
 func TestGetRules(t *testing.T) {
@@ -13,18 +10,13 @@ func TestGetRules(t *testing.T) {
 rule(name="test", sources=[], commands=["echo hey"], outputs=[])
 rule(name="publish", sources=["test"], commands=["echo bye"], outputs=[])
 `
-	rules, err := GetRules("BUILD", strings.NewReader(data))
+	rules, err := GetRules("BUILD", strings.NewReader(data), "all")
 	if err != nil {
 		t.Fatalf("expected `rule` function to work: %w", err)
 	}
 
-	testRule, ok := rules["test"]
-	if !ok {
-		t.Fatal("expected rulesDag to have a rule with test id")
-	}
-
-	if len(testRule.Children) != 0 {
-		t.Errorf("expected test's children to be empty, but got %v", testRule.Children)
+	if len(rules) != 1 {
+		t.Fatalf("expected rules to only be source rules, but got: %v", rules)
 	}
 
 	publishRule, ok := rules["publish"]
@@ -32,16 +24,39 @@ rule(name="publish", sources=["test"], commands=["echo bye"], outputs=[])
 		t.Fatal("expected rulesDag to have a rule with publish id")
 	}
 
-	if !reflect.DeepEqual(publishRule.Children, []*rule.Rule{testRule}) {
+	if len(publishRule.Children) != 1 {
+		t.Fatalf("expected publishRule to only have 1 child, but got: %v", publishRule.Children)
+	}
+
+	if publishRule.Children[0].Name != "test" {
 		t.Errorf("expected publish's children to have test, but had %v", publishRule.Children)
 	}
 }
 
+func TestGetRulesReturnsTargetRuleWhenNotAll(t *testing.T) {
+	data := `
+rule(name="build", sources=[], commands=["true"], outputs=[])
+rule(name="test", sources=["build"], commands=["echo hey"], outputs=[])
+rule(name="publish", sources=["build"], commands=["echo bye"], outputs=[])
+`
+	rules, err := GetRules("BUILD", strings.NewReader(data), "publish")
+	if err != nil {
+		t.Fatalf("expected `rule` function to work: %w", err)
+	}
+
+	if _, ok := rules["publish"]; !ok {
+		t.Fatalf("expected rules to have publish rule, but got: %v", rules)
+	}
+
+	if len(rules) != 1 {
+		t.Errorf("expected rules to only contain target rule, but got: %v", rules)
+	}
+}
 func TestGetRulesReturnsErrorWhenItFailsToRunBuildFile(t *testing.T) {
 	data := `
 rule(invalid_args=1)
 `
-	_, err := GetRules("BUILD", strings.NewReader(data))
+	_, err := GetRules("BUILD", strings.NewReader(data), "all")
 	if err == nil {
 		t.Error("should return error if failed to run BUILD file")
 	}
@@ -53,7 +68,7 @@ rule(name="test", sources=[], commands=["echo hey"], outputs=[])
 rule(name="test", sources=[], commands=["echo hey"], outputs=[])
 `
 
-	_, err := GetRules("BUILD", strings.NewReader(data))
+	_, err := GetRules("BUILD", strings.NewReader(data), "all")
 	if err == nil {
 		t.Error("should return error when duplicate rule name is found")
 	}
@@ -68,12 +83,28 @@ func TestGetRulesReturnsErrorWhenSourceDoesntExist(t *testing.T) {
 rule(name="test", sources=["build"], commands=["echo hey"], outputs=[])
 `
 
-	_, err := GetRules("BUILD", strings.NewReader(data))
+	_, err := GetRules("BUILD", strings.NewReader(data), "all")
 	if err == nil {
 		t.Error("should return error when rule name is not found")
 	}
 
 	if !strings.Contains(err.Error(), "not found") {
 		t.Errorf("error should mention not found: %v", err)
+	}
+}
+
+func TestGetRulesReturnsErrorWhenTargetDoesntExist(t *testing.T) {
+	data := `
+rule(name="build", sources=[], commands=["true"], outputs=[])
+rule(name="test", sources=["build"], commands=["echo hey"], outputs=[])
+rule(name="publish", sources=["build"], commands=["echo bye"], outputs=[])
+`
+	_, err := GetRules("BUILD", strings.NewReader(data), "deploy")
+	if err == nil {
+		t.Fatal("expected an error when target doesn't exist")
+	}
+
+	if !strings.Contains(err.Error(), "target deploy not found") {
+		t.Errorf("expected error message to container target deploy not found, but got: %s", err.Error())
 	}
 }

internal/build/build.go
@@ -17,20 +17,9 @@ func RunBuildFile(fs afero.Fs, buildFileName, buildTarget string, cacheDirectory
 		return err
 	}
 
-	rules, err := analyze.GetRules(buildFileName, buildFile)
+	rules, err := analyze.GetRules(buildFileName, buildFile, buildTarget)
 	if err != nil {
 		return err
-	}
-
-	if buildTarget != "all" {
-		targetRule, ok := rules[buildTarget]
-		if !ok {
-			return fmt.Errorf("target %s not found", buildTarget)
-		}
-
-		rules = map[string]*rule.Rule{
-			targetRule.Name: targetRule,
-		}
 	}
 
 	for _, rule := range rules {

commit 920b0328a5fe3d464d345468e3579447776ac84e

fix(internal/graph): traverse rule dependencies
This was broken in f1e29f4d186f5a8ed7312957e72132f8cd5d24c5. Before that
commit, graph used to get all rules, but afterwards it got all sources
(rules without any dependent rules). So, now traverse rules so that
graph is every rule.

internal/graph/graph.go
@@ -43,12 +43,21 @@ func getDotGraph(rules map[string]*rule.Rule) string {
 	dagString := "digraph g {\n"
 
 	for _, rule := range rules {
-		for _, child := range rule.Children {
-			dagString = dagString + fmt.Sprintf("  \"%s\" -> \"%s\";\n", rule.Name, child.Name)
-		}
+		dagString = dagString + getEdges(rule)
 	}
 
 	dagString = dagString + "}"
 
 	return dagString
+}
+
+func getEdges(rule *rule.Rule) string {
+	edgesString := ""
+
+	for _, child := range rule.Children {
+		edgesString = edgesString + getEdges(child) + fmt.Sprintf("  \"%s\" -> \"%s\";\n", rule.Name, child.Name)
+
+	}
+
+	return edgesString
 }